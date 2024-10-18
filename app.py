8c8921e8d247d75287289d7cc615c82c4834ec8a
Limetop <alexandr246@vk.com>
2024-10-18 16:32:27 +0300

    gg

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
+    level: so.Mapped[int] = so.mapped_column()
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
