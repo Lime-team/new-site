3fc68916659f786edec1e26b628d4bf2642e8cc9
Limetop <alexandr246@vk.com>
2024-10-24 15:34:52 +0300

    abc

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask_login import LoginManager, UserMixin
 
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_migrate import Migrate
 
 from flask import redirect, url_for, render_template, flash, request
@@ -23,6 +24,7 @@ from typing import Optional
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 import os
@@ -37,9 +39,10 @@ app.secret_key = os.getenv('FLASK-SECRET-KEY')
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.abspath(
-    os.path.dirname(__file__)), 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = (f"mysql://{os.getenv('DB-USERNAME')}:{os.getenv('DB-PASSWORD')}"
+                                         f"@{os.getenv('DB-URL')}/{os.getenv('DB-NAME')}")
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 load_dotenv()
@@ -56,23 +59,26 @@ def index():
 
 @app.route('/about-me')
 def about_me():
-    return render_template('about_me.html')
+    return render_template('about_me.html', name="Обо мне")
 
 
 @app.route('/discord-server')
 def discord_server():
-    return render_template('discord_server.html')
+    return render_template('discord_server.html', name="Discord сервер")
 
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.html', pages=['static/blog_files/a.html'])
+    return render_template('blog.html', pages=[], name="Блог")
 
 
 @app.route('/admin')
 @login_required
 def admin():
-    return render_template('admin.html')
+    if current_user.is_admin():
+        return render_template('admin.html', name="Админ-панель")
+    else:
+        return error_403('')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -81,7 +87,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data)
+        user = User(username=form.username.data, level=0)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
@@ -106,25 +112,33 @@ def login():
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('blog')
         return redirect(next_page)
-    return render_template('login.html', form=form)
+    return render_template('login.html', form=form, name="Вход")
 
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET'])
 def logout():
     logout_user()
-    return redirect(url_for('blog'))
+    next_page = request.args.get('next')
+    if not next_page or urlsplit(next_page).netloc != '':
+        next_page = url_for('blog')
+    return redirect(next_page)
 
 
 # errors
 
 @app.errorhandler(401)
 def error_401(error):
-    return render_template('401.html')
+    return render_template('401.html', name="Ошибка 401"), 401
 
 
 @app.errorhandler(404)
 def error_404(error):
-    return render_template('404.html')
+    return render_template('404.html', name="Ошибка 404"), 404
+
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('403.html', name="Ошибка 403"), 403
 
 
 # db
@@ -144,6 +158,12 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def change_level(self, level):
+        self.level = level
+
+    def is_admin(self):
+        return db.session.scalar(sa.select(User.level).where(User.id == self.id)) == 1
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -151,7 +171,7 @@ class User(UserMixin, db.Model):
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     post_name: so.Mapped[str] = so.mapped_column(sa.String(50))
-    body: so.Mapped[str] = so.mapped_column(sa.String())
+    body: so.Mapped[str] = so.mapped_column(sa.String(1000))
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
@@ -184,7 +204,14 @@ class RegistrationForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(
             User.username == username.data))
         if user is not None:
-            raise ValidationError('Используй другое имяs')
+            raise ValidationError('Используй другое имя!')
+
+
+class PostForm(FlaskForm):
+    username = StringField('Название статьи (отображается в списке статей и самой статье)', validators=[DataRequired()])
+    post = TextAreaField('Текст статьи', validators=[
+        DataRequired()])
+    submit = SubmitField('Опубликовать')
 
 
 # load user
@@ -201,7 +228,7 @@ if __name__ == "__main__":
 
     # for server use:
     # from waitress import serve
-    # serve(app, host="::1", port=80, _quiet=False)
+    # serve(app, host="::1", _quiet=False)
 
     # for tests:
     app.run(host="0.0.0.0", port=80, debug=True)
